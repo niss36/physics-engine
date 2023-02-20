d83871ca5d670fdc5f2f973f5003123ac30ed24e
niss36 <nissim.chekroun@gmail.com>
2023-02-20 23:44:43 +0000

    feat: use coefficient of restitution to model elastic (bouncy) collisions

--- src/simulation/world.rs
+++ src/simulation/world.rs
@@ -73,14 +73,14 @@ impl World {
         }
     }
 
-    fn handle_collisions(&mut self, elapsed: &Duration) {
+    fn handle_collisions(&mut self) {
         let num_bodies = self.bodies.len();
         for i in 0..num_bodies {
             let (head, tail) = self.bodies.split_at_mut(i + 1);
             let this = &mut head[i];
 
             for that in tail {
-                handle_collision(this, that, elapsed);
+                handle_collision(this, that);
             }
         }
     }
@@ -93,12 +93,12 @@ impl World {
 
     pub fn tick(&mut self, elapsed: Duration) {
         self.apply_gravity(&elapsed);
-        self.handle_collisions(&elapsed);
+        self.handle_collisions();
         self.integrate_bodies(&elapsed);
     }
 }
 
-fn handle_collision(this: &mut Body, that: &mut Body, elapsed: &Duration) {
+fn handle_collision(this: &mut Body, that: &mut Body) {
     if !fast_collision_check(this, that) {
         return;
     }
@@ -112,17 +112,26 @@ fn handle_collision(this: &mut Body, that: &mut Body, elapsed: &Duration) {
     let this_body = this.as_mut();
     let that_body = that.as_mut();
 
+    apply_impulse(this_body, that_body, &contact);
+}
+
+fn apply_impulse(this_body: &mut BaseBody, that_body: &mut BaseBody, contact: &Contact) {
     let relative_velocity = &that_body.velocity - &this_body.velocity;
     let relative_velocity_dot_normal = relative_velocity.dot_product(&contact.normal);
 
-    let to_remove =
-        relative_velocity_dot_normal + 0.4 + (contact.distance + 1.) / elapsed.as_secs_f64();
-
-    if to_remove >= 0. {
+    if relative_velocity_dot_normal > 0. {
         return;
     }
 
-    let impulse = &contact.normal * (to_remove / (this_body.inverse_mass + that_body.inverse_mass));
+    let coefficient_of_restitution = f64::min(
+        this_body.coefficient_of_restitution,
+        that_body.coefficient_of_restitution,
+    );
+
+    let impulse_amount = (1. + coefficient_of_restitution) * relative_velocity_dot_normal
+        / (this_body.inverse_mass + that_body.inverse_mass);
+
+    let impulse = &contact.normal * impulse_amount;
 
     this_body.velocity += &(&impulse * this_body.inverse_mass);
     that_body.velocity -= &(&impulse * that_body.inverse_mass);
