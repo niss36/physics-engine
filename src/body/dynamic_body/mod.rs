6366f0660c2551d6b4b36527f927988dd5f2fa69
niss36 <nissim.chekroun@gmail.com>
2023-07-01 11:42:13 +0100

    refactor: move dynamic bodies to `dynamic_body` module

---
+++ src/body/dynamic_body/mod.rs
@@ -0,0 +1,36 @@
+mod base;
+mod circle;
+mod line;
+mod rectangle;
+
+pub use base::*;
+pub use circle::*;
+pub use line::*;
+pub use rectangle::*;
+
+#[derive(Debug, Clone, Copy)]
+pub enum DynamicBody {
+    Circle(Circle),
+    Line(Line),
+    Rectangle(Rectangle),
+}
+
+impl AsRef<BaseDynamicBody> for DynamicBody {
+    fn as_ref(&self) -> &BaseDynamicBody {
+        match self {
+            Self::Circle(circle) => circle.as_ref(),
+            Self::Line(line) => line.as_ref(),
+            Self::Rectangle(rectangle) => rectangle.as_ref(),
+        }
+    }
+}
+
+impl AsMut<BaseDynamicBody> for DynamicBody {
+    fn as_mut(&mut self) -> &mut BaseDynamicBody {
+        match self {
+            Self::Circle(circle) => circle.as_mut(),
+            Self::Line(line) => line.as_mut(),
+            Self::Rectangle(rectangle) => rectangle.as_mut(),
+        }
+    }
+}
--- src/body/mod.rs
+++ src/body/mod.rs
@@ -1,36 +1,3 @@
-pub mod base;
-pub mod circle;
-pub mod line;
-pub mod rectangle;
+mod dynamic_body;
 
-pub use base::*;
-pub use circle::*;
-pub use line::*;
-pub use rectangle::*;
-
-#[derive(Debug, Clone, Copy)]
-pub enum DynamicBody {
-    Circle(Circle),
-    Line(Line),
-    Rectangle(Rectangle),
-}
-
-impl AsRef<BaseDynamicBody> for DynamicBody {
-    fn as_ref(&self) -> &BaseDynamicBody {
-        match self {
-            Self::Circle(circle) => circle.as_ref(),
-            Self::Line(line) => line.as_ref(),
-            Self::Rectangle(rectangle) => rectangle.as_ref(),
-        }
-    }
-}
-
-impl AsMut<BaseDynamicBody> for DynamicBody {
-    fn as_mut(&mut self) -> &mut BaseDynamicBody {
-        match self {
-            Self::Circle(circle) => circle.as_mut(),
-            Self::Line(line) => line.as_mut(),
-            Self::Rectangle(rectangle) => rectangle.as_mut(),
-        }
-    }
-}
+pub use dynamic_body::*;
